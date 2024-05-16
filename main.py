c276d0e712fb89ddd643b8e9b62faa933f8189c7
nus3206 <nus32063206@gmail.com>
2024-05-16 17:35:24 +0700

    test build

--- main.py
+++ main.py
@@ -4,55 +4,35 @@ import asyncio
 from telethon import TelegramClient, events
 from telethon.errors import SessionPasswordNeededError
 from dotenv import load_dotenv
-from Services import process_bonus_code
-import pytesseract
-from PIL import ImageGrab
 import re
-import uuid
 
 load_dotenv()
 
-# Retrieve and validate API credentials and other parameters from environment variables
-def get_env_variable(var_name, default=None):
-    value = os.environ.get(var_name)
-    if value is None:
-        if default is None:
-            raise ValueError(f"Environment variable {var_name} is not set")
-        return default
-    return value
-
-api_id = int(get_env_variable('APP_API_ID'))
-api_hash = get_env_variable('APP_API_HASH')
+api_id = int(os.environ.get('API_ID'))
+api_hash = os.environ.get('API_HASH')
 source_channel_ids = [
-    int(get_env_variable('SOURCE_CHANNEL_ID')),
-    int(get_env_variable('TEST_CHANNEL_ID')),
-    int(get_env_variable('TELEGRAM_CHANNEL_ID'))
+    int(os.environ.get('SOURCE_CHANNEL_ID')),
+    int(os.environ.get('TELEGRAM_CHANNEL_ID')),
+    int(os.environ.get('TEST_CHANNEL_ID'))
 ]
-destination_channel_id = int(get_env_variable('DESTINATION_CHANNEL_ID'))
-phone_number = get_env_variable('APP_YOUR_PHONE')
-user_password = get_env_variable('APP_YOUR_PWD')
+destination_channel_id = int(os.environ.get('DESTINATION_CHANNEL_ID'))
+phone_number = os.environ.get('APP_YOUR_PHONE')
+user_password = os.environ.get('APP_YOUR_PWD')
+telegram_channel_id = int(os.environ.get('TELEGRAM_CHANNEL_ID'))
+
 
 async def get_input(prompt, default=None):
-    if default is not None:
-        return default
-    else:
-        return input(prompt).strip()
+    return default if default is not None else input(prompt).strip()
 
-async def get_login_code():
-    # Take a screenshot
-    screenshot = ImageGrab.grab()
-    
-    # Perform OCR on the screenshot
-    text = pytesseract.image_to_string(screenshot)
-    
-    # Parse the text to find the login code
-    pattern = r"Login code: (\d+)"
-    match = re.search(pattern, text)
-    if match:
-        code = match.group(1)
-        return code
-    else:
-        return None
+async def get_login_code(channel_id):
+    async with TelegramClient('anon', api_id, api_hash) as client:
+        async for message in client.iter_messages(channel_id, limit=1):
+            text = message.text
+            match = re.search(r"Login code: (\d+)", text)
+            if match:
+                return match.group(1)
+            else:
+                return None
 
 class MessageForwarder:
     def __init__(self, api_id, api_hash, phone_number, source_channel_ids, destination_channel_id, user_password):
@@ -62,12 +42,9 @@ class MessageForwarder:
             os.makedirs(session_dir)
 
         # Use a unique session name
-        unique_session_name = f"{session_dir}/session_{phone_number}_{uuid.uuid4()}"
-        session_path = os.path.join(session_dir, unique_session_name + '.session')
+        self.session_name = f"{session_dir}/session_{phone_number}"
+        self.session_path = self.session_name + '.session'
         
-        if os.path.exists(session_path):
-            os.remove(session_path)  # Remove old session file if it exists
-
         self.api_id = api_id
         self.api_hash = api_hash
         self.phone_number = phone_number
@@ -75,10 +52,31 @@ class MessageForwarder:
         self.destination_channel_id = destination_channel_id
         self.user_password = user_password
 
-        self.client = TelegramClient(unique_session_name, api_id, api_hash)
+        self.client = TelegramClient(self.session_name, api_id, api_hash)
         self.connected = False
 
+    async def check_session_validity(self):
+        try:
+            if not self.client.is_connected():
+                await self.client.connect()
+            return await self.client.is_user_authorized()
+        except Exception as e:
+            print(f"An error occurred while checking session validity: {e}")
+            return False
+
     async def connect(self):
+        if os.path.exists(self.session_path):
+            if not await self.check_session_validity():
+                try:
+                    os.remove(self.session_path)
+                except PermissionError:
+                    print("Unable to remove session file. Waiting for a moment and retrying...")
+                    await asyncio.sleep(1)  # Wait for a short duration
+                    try:
+                        os.remove(self.session_path)  # Retry removing the file
+                    except PermissionError:
+                        print("Unable to remove session file even after retrying.")
+                self.client = TelegramClient(self.session_name, self.api_id, self.api_hash)
         await self.client.connect()
         self.connected = True
 
@@ -92,11 +90,11 @@ class MessageForwarder:
 
         if not await self.client.is_user_authorized():
             await self.client.send_code_request(self.phone_number)
-            code = await get_input('Enter the code: ')
+            code = await get_login_code(telegram_channel_id) or await get_input('Enter the code: ')
             try:
                 await self.client.sign_in(self.phone_number, code)
             except SessionPasswordNeededError:
-                password = await get_input('Two-step verification is enabled. Please enter your password: ')
+                password = self.user_password or await get_input('Two-step verification is enabled. Please enter your password: ')
                 await self.client.sign_in(password=password)
             except Exception as e:
                 print(f"An error occurred during sign-in: {e}")
@@ -119,11 +117,10 @@ class MessageForwarder:
                     @self.client.on(events.NewMessage(chats=source_channel_ids))
                     async def message_handler(event):
                         try:
-                            print(f"New message received: {event.message.text}")  # Debug statement
+                            print(f"New message received: {event.message.text}")  
                             await self.client.forward_messages(self.destination_channel_id, event.message)
-                            print(f"Message forwarded to {self.destination_channel_id}")  # Debug statement
-                            await process_bonus_code(apiEndpoints, event.message.text)
-                            print("process_bonus_code called successfully")  # Debug statement
+                            print(f"Message forwarded to {self.destination_channel_id}") 
+                            # Call function to process bonus code here
                         except Exception as e:
                             print(f"An error occurred while processing the message: {e}")
 
@@ -146,21 +143,17 @@ async def ping_endpoint(endpoint):
             async with session.get(endpoint) as response:
                 if response.status == 200:
                     print(f"Endpoint {endpoint} is reachable.")
-                    apiEndpoints.append(endpoint)
                 else:
                     print(f"Endpoint {endpoint} is not reachable. Status code: {response.status}")
     except aiohttp.ClientError as e:
         print(f"Error connecting to {endpoint}: {e}")
 
-def process_input(input_str):
-    return input_str.lower()
-
 async def main():
     forwarder = MessageForwarder(api_id, api_hash, phone_number, source_channel_ids, destination_channel_id, user_password)
     api_endpoints = [
-        get_env_variable('API_ENDPOINT_1'),
-        get_env_variable('API_ENDPOINT_2'),
-        get_env_variable('API_ENDPOINT_3')
+        os.environ.get('API_ENDPOINT_1'),
+        os.environ.get('API_ENDPOINT_2'),
+        os.environ.get('API_ENDPOINT_3')
     ]
 
     tasks = [ping_endpoint(endpoint) for endpoint in api_endpoints]
@@ -174,7 +167,6 @@ async def main():
         if choice == "1":
             await forwarder.list_chats()
         elif choice == "2":
-            print(apiEndpoints)
             try:
                 await forwarder.forward_new_messages()
             except Exception as e:
@@ -183,7 +175,6 @@ async def main():
             print("Invalid choice")
 
 if __name__ == "__main__":
-    apiEndpoints = []  # Initialize global variable
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
